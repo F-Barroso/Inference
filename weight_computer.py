2fe725b24da00503fbb8924a3373904dced5bb29
Filipe Barroso <61795671+F-Barroso@users.noreply.github.com>
2023-12-29 15:04:30 +0000

    Update weight_computer.py

--- weight_computer.py
+++ weight_computer.py
@@ -7,15 +7,15 @@ def weight_num_writer(df, states, order,filename='weights_num'):
     '''Computes numerical weights and writes them in a txt file.'''
     
     file = open(filename+'.txt', 'w')
-                 
+    dsize = len(df)
     for (var1,var2) in it.permutations(states.keys(),2):
         if order[var1]<order[var2]:
             for (st_var1,st_var2) in it.product(*[states[var1],states[var2]]):
-                PA = (df[var1]==st_var1).sum()/len(df)
+                PA = (df[var1]==st_var1).sum()/dsize
                 if PA == 0 or PA==1:
                     continue
-                PB = (df[var2]==st_var2).sum()/len(df)
-                PAB= ((df[var1]==st_var1)&(df[var2]==st_var2)).sum()/len(df)
+                PB = (df[var2]==st_var2).sum()/dsize
+                PAB= ((df[var1]==st_var1)&(df[var2]==st_var2)).sum()/dsize
                 file.write( str(var1)+";"+str(var2)+";"+str(st_var1)+";"+str(st_var2) + ":" + str((PAB - PA*PB)/(PA*(1-PA)))+"\n" )
           
         
@@ -36,15 +36,16 @@ def weight_theo_writer(G, states, states_prob):
 def npmi_num_writer(df, states, order):
     '''Computes numerical NPMI and writes them in a txt file.'''
     
+    dsize = len(df)
     file = open('mi_num.txt', 'w')
     for (var1,var2) in it.permutations(states.keys(),2):
         if order[var1]<order[var2]:
             for (st_var1,st_var2) in it.product(*[states[var1],states[var2]]):
-                PA = (df[var1]==st_var1).sum()/len(df)
-                PB = (df[var2]==st_var2).sum()/len(df)
+                PA = (df[var1]==st_var1).sum()/dsize
+                PB = (df[var2]==st_var2).sum()/dsize
                 if PA==0 or PB==0:
                     continue
-                PAB= ((df[var1]==st_var1)&(df[var2]==st_var2)).sum()/len(df)
+                PAB= ((df[var1]==st_var1)&(df[var2]==st_var2)).sum()/dsize
             
                 if PAB==0 and PA!=0 and PB!=0:
                     file.write( str(var1)+";"+str(var2)+";"+str(st_var1)+";"+str(st_var2) + ":" + str(-1.)+"\n" )
@@ -56,13 +57,14 @@ def pmi_num_writer(df, states):
     '''Computes numerical PMI and writes them in a txt file.'''
     
     file = open('pmi_num.txt', 'w')
-                 
+
+    dsize = len(df)
     for (var1,var2) in it.permutations(states.keys(),2):
         if var1<var2:
             for (st_var1,st_var2) in it.product(*[states[var1],states[var2]]):
-                PA = (df[var1]==st_var1).sum()/len(df)
-                PB = (df[var2]==st_var2).sum()/len(df)
-                PAB= ((df[var1]==st_var1)&(df[var2]==st_var2)).sum()/len(df)
+                PA = (df[var1]==st_var1).sum()/dsize
+                PB = (df[var2]==st_var2).sum()/dsize
+                PAB= ((df[var1]==st_var1)&(df[var2]==st_var2)).sum()/dsize
             
                 if PAB==0 and PA!=0 and PB!=0:
                     file.write( str(var1)+";"+str(var2)+";"+str(st_var1)+";"+str(st_var2) + ":" + str(-np.inf)+"\n" )
