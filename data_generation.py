795a8d7ee266c7d3b76b8edef46f1b3b9c65665a
Filipe Barroso <61795671+F-Barroso@users.noreply.github.com>
2024-01-04 18:46:14 +0000

    Update data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -60,20 +60,20 @@ def probabiliter_controlled(G, states):
         problist = np.array([[betas[i] * (1-r[u]) for i in range(d)] for u in range(nj)])+alphas # P(i|u)
         cumlist=np.cumsum(problist,axis=1)
 
-        i=0
         problist_, cumlist_ = np.zeros(np.size(problist)), np.zeros(np.size(problist))
         df = pd.DataFrame(it.product(states[node],*[states[parent] for parent in parents]), columns=[node]+parents)
-        for parent_states in it.product(*[states[parent] for parent in parents]): #Parallizable?
-
-            where=(df[parents]==parent_states).all(axis=1)
+        
+        lps = sum([1 for cnt in it.product(*[states[parent] for parent in parents])]) #number of products of parent_states
+        #Loops over the number of products over parent states; if zero it has a single cycle with where selecting all lines (thus all node_states)
+        for i in range(lps):
+            where = [i + lps*j for j in range(d)] # lines in df (thus different node_states) with the same parent_states
             problist_[where] = problist[i]
             cumlist_[where] = cumlist[i]
-            i+=1
 
         df['Probability'] = problist_
         df['Cumulative'] = cumlist_
         probs[node] = df
-        
+                
     return probs
     
 ########################################################################
@@ -117,12 +117,11 @@ def probabiliter_forced(G, states):
         problist_ = np.zeros(np.size(problist))
         cumlist_ = np.zeros(np.size(problist))
                 
-        for parent_states in np.unique(df[parents],axis=0):
-            
-            where=(df[parents]==parent_states).all(axis=1)
+        lps = sum([1 for cnt in it.product(*[states[parent] for parent in parents])]) #number of product of parent states
+        for i in range(lps):
+            where = [i + lps*j for j in range(d)] # lines in df with the same parent states; the states repeat after lps lines
             problist_[where] = problist[i]
             cumlist_[where] = cumlist[i]
-            i+=1
              
         df['Probability'] = problist_
         df['Cumulative'] = cumlist_
@@ -137,26 +136,30 @@ def generator(G, states, states_prob, n):
     '''Generates n lines of data consistent with a given DAG, node states and their probabilities.'''
     data = {} #initialize a dictionary
     
+    #These two loops are computationally equivalent to a single loop over topologically ordered nodes
     for gen in nx.topological_generations(G): #loop over DAG generations
-        
-        for node in gen:
-            parents = list(G.predecessors(node))
-            df = states_prob[node].copy()
-        
-            rng = rd.random(n) #generate random numbers, used to determine states according probability distributions
-            st_counter=np.zeros(n, dtype=int)
+        for node in gen: #loop over the nodes in the generation
             
-            for node_state in states[node]:
+            parents = list(G.predecessors(node))
+            df = states_prob[node] #dataframe with the conditional probabilities to a node
+        
+            rng = rd.random(n) #generate n random numbers, used to determine states according probability distributions
+            st_counter=np.zeros(n, dtype=int) #array that shall contain the n realizations of the node
+            
+            lps = sum([1 for cnt in it.product(*[states[parent] for parent in parents])]) #number of product of parent states
+            #Loops over the number of lines of df; each maps to an unique system state=node_state+parent_states
+            for i in range(len(df)):
+                
+                truths = np.ones(n,dtype=bool) #Initialize array of Trues:
+                #each position of the array shall indicate after the loop if the realizations already generated for the parent states
+                #are compatible with the parent_states in line i of df. If the node is orphan, the array will remain unchanged.
+                for j in range(len(parents)):
+                    truths &= (data[parents[j]] == df.loc[i][parents[j]]) #find which realizations are not compatible with parent_states
                     
-                for parent_states in np.array(df.loc[df[node]==node_state][parents]):
-                                                
-                    truths = np.ones(n).astype(bool)
-                    for i in range(len(parent_states)): #find the positions that satisfy the parent states
-                        truths &= (data[parents[i]] == parent_states[i])
-                                                
-                    #cummulatively find the states in the truth positions    
-                    st_counter[truths] += (df.loc[(df[[node,*parents]]==(node_state,*parent_states)).all(axis=1)].Cumulative.values[0]
-                                           < rng[truths])
+                #Update the st_counter in the positions that are compatible with the already generated parent states.
+                #Note: at the end of current loop, one and only one value will be assigned to each position of st_counter
+                st_counter[truths] += df.loc[i].Cumulative < rng[truths]
 
-            data[node]=st_counter
+            data[node]=st_counter #add an entry to the dictionary with the realizations for the node
+            
     return pd.DataFrame(data)
