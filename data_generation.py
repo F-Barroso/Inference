ff9f05230516d9675b9b394658049cac2974aa4c
Filipe Barroso <61795671+F-Barroso@users.noreply.github.com>
2024-01-22 12:10:21 +0000

    Update data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -11,8 +11,7 @@ def stater(G, min_states=2, max_states=4):
 ########################################################################
 
 def probabiliter_controlled(G, states):
-    '''Returns a dictionary of pandas DataFrames containing probabilities generated from random parameters for each combination of states
-    and the respective cumulative distributions.'''
+    '''Returns a dictionary of arrays containing probabilities generated from random parameters for each combination of states and the respective cumulative distributions.'''
    
     probs = {} #dictionary to hold the dataframes
     
@@ -30,18 +29,14 @@ def probabiliter_controlled(G, states):
         problist = np.array([[betas[i] * (1-r[u]) for i in range(d)] for u in range(lps)])+alphas # P(i|u)
         cumlist=np.cumsum(problist,axis=1)
 
-        problist_, cumlist_ = np.zeros(lps*d), np.zeros(lps*d)
+        cumlist_ = np.zeros(lps*d)
         
         #Loops over the number of products over parent states; if zero it has a single cycle with where selecting all lines (thus all node_states)
         for i in range(lps):
-            where = [i + lps*j for j in range(d)] # lines in df (thus different node_states) with the same parent_states
-            problist_[where] = problist[i]
+            where = [i + lps*j for j in range(d)] # lines in cumlist_ (thus different node_states) with the same parent_states
             cumlist_[where] = cumlist[i]
 
-        df = pd.DataFrame() #each index is mapped to the the iterator it.product(states[node],*[states[parent] for parent in parents])
-        df['Probability'] = problist_
-        df['Cumulative'] = cumlist_
-        probs[node] = df
+        probs[node] = cumlist_
                 
     return probs
     
@@ -56,25 +51,25 @@ def generator(G, states, states_prob, n):
         for node in gen: #loop over the nodes in the generation
             
             parents = list(G.predecessors(node))
-            df = states_prob[node] #dataframe with the conditional probabilities to a node
+            probs = states_prob[node] #array with the conditional probabilities to a node
         
             rng = rd.random(n) #generate n random numbers, used to determine states according probability distributions
             st_counter=np.zeros(n, dtype=int) #array that shall contain the n realizations of the node
             
-            i=0 #counter of system states/lines of df
+            i=0 #counter of system states/lines of probs
             #Loops over the combinations
             for sys_state in it.product(states[node],*[states[parent] for parent in parents]):
                 
                 truths = np.ones(n,dtype=bool) #Initialize array of Trues:
                                 
                 #each position of the array shall indicate after the loop if the realizations already generated for the parent states
-                #are compatible with the parent_states in line i of df. If the node is orphan, the array will remain unchanged.
+                #are compatible with the parent_states in line i of probs. If the node is orphan, the array will remain unchanged.
                 for j in range(len(parents)):
                     truths &= (data[parents[j]] == sys_state[1+j]) #find which realizations are not compatible with parent_states
                         
                 #Update the st_counter in the positions that are compatible with the already generated parent states.
                 #Note: at the end of current loop, one and only one value will be assigned to each position of st_counter
-                st_counter[truths] += df.loc[i].Cumulative < rng[truths]
+                st_counter[truths] += probs[i] < rng[truths]
                 i+=1
 
             data[node]=st_counter #add an entry to the dictionary with the realizations for the node
