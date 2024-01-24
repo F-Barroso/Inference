4b7de9f142a472e47fde07f07e29e44f3cc4ae0c
Filipe Barroso <61795671+F-Barroso@users.noreply.github.com>
2024-01-24 14:55:14 +0000

    Update SyntheticData_Dense.py

--- SyntheticData_Dense.py
+++ SyntheticData_Dense.py
@@ -11,94 +11,12 @@ import time
 
 from controlled_zeros import *
 from data_generation import *
+from triangulation import *
 from weight_computer import *
 
 from castle.common import GraphDAG, independence_tests
 from castle.metrics import MetricsDAG
 from castle.algorithms import PC
-
-import matplotlib
-font = {'size'   : 11}
-matplotlib.rc('font', **font)
-
-def condition(node_list, arr ,m):
-    H = nx.DiGraph()
-    H.add_nodes_from(node_list)
-    H.add_edges_from(np.unique(arr[m:],axis=1))
-    return nx.is_connected(H.to_undirected())
-
-def binary_search(node_list, arr):
-    '''arr - sorted array'''
-    L=0
-    R=len(arr)-1
-    while L<=R:
-        if L==R:
-            return L
-        
-        m = int((L+R)/2)+1
-        if not condition(node_list, arr, m): #is it not connected?
-            R = m-1
-        else:
-            L = m
-    return "fail"
-
-def triangulation(df, edge_list, thres, states):
-    DAG_w2 = nx.DiGraph()
-    DAG_w2.add_nodes_from(df.columns)
-    DAG_w2.add_edges_from(edge_list)
-    
-    for b in np.array(DAG_w2.nodes)[np.array(DAG_w2.in_degree)[:,1].astype("int")>1]:
-        parents = np.array(list(DAG_w2.in_edges(b)))[:,0]
-        
-        for a in parents: #a-> b <-u (test a->b; b=node)
-            if (a,b) not in DAG_w2.edges: #was it already removed?
-                continue
-            #Note: it.permutations(parents,2) can't be used in conjuction with the last break since that will cause the cycle to skip triangles for different a's.
-            for c in parents[parents!=a]:
-                #print(b,a,parents[parents!=a])
-                survives=False
-
-                for st_varB,st_varA,st_varC in it.product(*[states[b],states[a],states[c]]):
-                    PC = (df[c]==st_varC).sum()
-                    PAC= ((df[a]==st_varA)&(df[c]==st_varC)).sum()
-                    PBC= ((df[b]==st_varB)&(df[c]==st_varC)).sum()
-                    PABC= ((df[b]==st_varB)&(df[a]==st_varA)&(df[c]==st_varC)).sum()
-                    if PAC!=0 and PAC!=PC: #conditioned to c
-                        if np.abs(PABC*PC-PAC*PBC)/(PAC*(PC-PAC)) > thres:
-                            survives=True
-                            break
-
-                if not survives:
-                    DAG_w2.remove_edge(a,b)
-                    #if (DAG_w2.out_degree(a) == 0):
-                    #    if order[a]<order[c]:
-                    #        DAG_w2.add_edge(a,c)
-                    #    if order[c]<order[a]:
-                    #        DAG_w2.add_edge(c,a)
-
-                    break
-    return DAG_w2
-
-def triangulation2(data, node_list, edge_list, thres):
-    key = {df.columns[i]:i for i in range(len(df.columns))}
-    DAG_w2 = nx.DiGraph()
-    DAG_w2.add_nodes_from(node_list)
-    DAG_w2.add_edges_from(edge_list)
-    
-    for b in np.array(DAG_w2.nodes)[np.array(DAG_w2.in_degree)[:,1].astype("int")>1]:
-        parents = np.array(list(DAG_w2.in_edges(b)))[:,0]
-        
-        for a in parents: #a-> b <-u (test a->b; b=node)
-            if (a,b) not in DAG_w2.edges: #was it already removed?
-                continue
-            #Note: it.permutations(parents,2) can't be used in conjuction with the last break since that will cause the cycle to skip triangles for different a's.
-            for c in parents[parents!=a]:
-                #print(b,a,parents[parents!=a])
-                if independence_tests.CITest.fisherz_test(data,key[a],key[b],[key[c]])[2] > thres: #does not survive
-                    DAG_w2.remove_edge(a,b)
-                    break
-                    
-    return DAG_w2
 
 for n_nodes in [20,40,60,80,100,120]:
 
