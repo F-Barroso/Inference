d564e134020be9a55446be6ea4d99884a7d99e09
Filipe Barroso <61795671+F-Barroso@users.noreply.github.com>
2024-02-02 11:25:07 +0000

    Update triangulation.py

--- triangulation.py
+++ triangulation.py
@@ -26,45 +26,41 @@ def binary_search(node_list, arr):
             L = m
     return "fail"
 
-def triangulation(df, edge_list, thres, states):
+def triangulation(data, node_list, edge_list, thres, states):
+    key = {node_list[i]:i for i in range(len(node_list))}
+
     DAG_w2 = nx.DiGraph()
-    DAG_w2.add_nodes_from(df.columns)
+    DAG_w2.add_nodes_from(node_list)
     DAG_w2.add_edges_from(edge_list)
     
     for b in np.array(DAG_w2.nodes)[np.array(DAG_w2.in_degree)[:,1].astype("int")>1]:
         parents = np.array(list(DAG_w2.in_edges(b)))[:,0]
         
         for a in parents: #a-> b <-u (test a->b; b=node)
-            if (a,b) not in DAG_w2.edges: #was it already removed?
-                continue
-            #Note: it.permutations(parents,2) can't be used in conjuction with the last break since that will cause the cycle to skip triangles for different a's.
-            for c in parents[parents!=a]:
-                #print(b,a,parents[parents!=a])
-                survives=False
-
-                for st_varB,st_varA,st_varC in it.product(*[states[b],states[a],states[c]]):
-                    PC = (df[c]==st_varC).sum()
-                    PAC= ((df[a]==st_varA)&(df[c]==st_varC)).sum()
-                    PBC= ((df[b]==st_varB)&(df[c]==st_varC)).sum()
-                    PABC= ((df[b]==st_varB)&(df[a]==st_varA)&(df[c]==st_varC)).sum()
-                    if PAC!=0 and PAC!=PC: #conditioned to c
-                        if np.abs(PABC*PC-PAC*PBC)/(PAC*(PC-PAC)) > thres:
-                            survives=True
-                            break
-
-                if not survives:
-                    DAG_w2.remove_edge(a,b)
-                    #if (DAG_w2.out_degree(a) == 0):
-                    #    if order[a]<order[c]:
-                    #        DAG_w2.add_edge(a,c)
-                    #    if order[c]<order[a]:
-                    #        DAG_w2.add_edge(c,a)
-
-                    break
+            if (a,b) in DAG_w2.edges: #was it already removed?
+                
+                #Note: it.permutations(parents,2) can't be used in conjuction with the last break since that will cause the cycle to skip triangles for different a's.
+                for c in parents[parents!=a]:
+                    #print(b,a,parents[parents!=a])
+                    survives=False
+                    
+                    for st_varB,st_varA,st_varC in it.product(*[states[b],states[a],states[c]]):
+                        PC = (Y[key[c]]==st_varC).sum()
+                        PAC= ((Y[key[a]]==st_varA)&(Y[key[c]]==st_varC)).sum()
+                        PBC= ((Y[key[b]]==st_varB)&(Y[key[c]]==st_varC)).sum()
+                        PABC= ((Y[key[b]]==st_varB)&(Y[key[a]]==st_varA)&(Y[key[c]]==st_varC)).sum()
+                        if PAC!=0 and PAC!=PC: #conditioned to c
+                            if np.abs(PABC*PC-PAC*PBC)/(PAC*(PC-PAC)) > thres:
+                                survives=True
+                                break
+                    if not survives:
+                        DAG_w2.remove_edge(a,b)
+                        break                        
     return DAG_w2
 
 def triangulation_fisher(data, node_list, edge_list, thres):
     '''Triangulation method tailored for fisher metric'''
+    key = {node_list[i]:i for i in range(len(node_list))}
     DAG_w2 = nx.DiGraph()
     DAG_w2.add_nodes_from(node_list)
     DAG_w2.add_edges_from(edge_list)
@@ -78,7 +74,7 @@ def triangulation_fisher(data, node_list, edge_list, thres):
                 #Note: it.permutations(parents,2) can't be used in conjuction with the last break since that will cause the cycle to skip triangles for different a's.
                 for c in parents[parents!=a]:
                     
-                    if independence_tests.CITest.fisherz_test(data,a,b,[c])[2] > thres: #does not survive
+                    if independence_tests.CITest.fisherz_test(data,key[a],key[b],[key[c]])[2] > thres: #does not survive
                         DAG_w2.remove_edge(a,b)
                         break
                     
