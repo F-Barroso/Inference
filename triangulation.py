1281973494349cc2b563d19ad48c3d23502993d7
Filipe Barroso <61795671+F-Barroso@users.noreply.github.com>
2024-02-02 10:47:44 +0000

    Update triangulation.py

--- triangulation.py
+++ triangulation.py
@@ -64,7 +64,7 @@ def triangulation(df, edge_list, thres, states):
     return DAG_w2
 
 def triangulation_fisher(data, node_list, edge_list, thres):
-    key = {node_list[i]:i for i in range(len(node_list))}
+    '''Triangulation method tailored for fisher metric'''
     DAG_w2 = nx.DiGraph()
     DAG_w2.add_nodes_from(node_list)
     DAG_w2.add_edges_from(edge_list)
@@ -73,13 +73,13 @@ def triangulation_fisher(data, node_list, edge_list, thres):
         parents = np.array(list(DAG_w2.in_edges(b)))[:,0]
         
         for a in parents: #a-> b <-u (test a->b; b=node)
-            if (a,b) not in DAG_w2.edges: #was it already removed?
-                continue
-            #Note: it.permutations(parents,2) can't be used in conjuction with the last break since that will cause the cycle to skip triangles for different a's.
-            for c in parents[parents!=a]:
-                #print(b,a,parents[parents!=a])
-                if independence_tests.CITest.fisherz_test(data,key[a],key[b],[key[c]])[2] > thres: #does not survive
-                    DAG_w2.remove_edge(a,b)
-                    break
+            if (a,b) in DAG_w2.in_edges: #was it already removed?
+                
+                #Note: it.permutations(parents,2) can't be used in conjuction with the last break since that will cause the cycle to skip triangles for different a's.
+                for c in parents[parents!=a]:
+                    
+                    if independence_tests.CITest.fisherz_test(data,a,b,[c])[2] > thres: #does not survive
+                        DAG_w2.remove_edge(a,b)
+                        break
                     
     return DAG_w2
