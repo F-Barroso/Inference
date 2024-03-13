a075754f3d425c91076c22553a804f13085355bc
Filipe Barroso <61795671+F-Barroso@users.noreply.github.com>
2024-03-13 15:43:14 +0000

    Update SyntheticData_Dense_NOrder.py

--- SyntheticData_Dense_NOrder.py
+++ SyntheticData_Dense_NOrder.py
@@ -230,7 +230,7 @@ for n_nodes in [20,40,60,80,100,120,200]:
         data[0,44] = FN/len(Gt.edges) #FNR = FN/P
         data[0,45] = (TP*TN - FP*FN)/np.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN)) #MCC
         
-        f = open("synthmeasuresDense_N.txt", "a+")
+        f = open("synthmeasuresDense_NOrder.txt", "a+")
         np.savetxt(f,data)
         f.close()
         del data
